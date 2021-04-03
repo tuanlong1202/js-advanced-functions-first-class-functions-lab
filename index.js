148350452f04647b0a3c00e2b9eb92024e562509
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-03 19:24:47 -0400

    JS Advance Function

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arrPassing = []) {
+    return arrPassing.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(arrPassing = []) {
+    return arrPassing.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(intMultiplier) {
+    return function (intFare) {
+        return intFare * intMultiplier;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrPassing = [], fnFirstOrLast) {
+    return fnFirstOrLast(arrPassing);
+}
